dba7b47b8f1b42a601fbbbb43ba933c06fce78df
anchi204 <163340813+anchi204@users.noreply.github.com>
2024-11-05 16:42:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import AddressForm from './AddressForm'; 
+
+
+function App() {
+  // const title='Welcome to the new blog';
+  // const likes=50;
+  // const person={name:'yoshi', age:30};
+  // const link="http://www.google.com";
+  return (
+      <div className="content">
+        <AddressForm/>
+      </div>
+  );
+}
+
+export default App;
